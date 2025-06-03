f82ef146936691490156c842ed57b09d937e3ac0
Ray <9025341@student.tcrmbo.nl>
2025-06-03 14:00:31 +0200

    homepage added

---
+++ project-escape-room/dbcon.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost"; 
+$username = "root";
+$password = ""; 
+$db = "escape-room";
+
+try {
+  $db_connection = new PDO("mysql:host=$server; dbname=$db", $username, $password);
+  $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Verbinding mislukt" . $e->getMessage();
+}
